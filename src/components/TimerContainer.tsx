f6d32098b840f25fc9a9472a5fffa485b59fe5b7
LimFull <full7002@naver.com>
2022-01-31 21:39:47 +0900

    화면 길이 수정

--- src/components/TimerContainer.tsx
+++ src/components/TimerContainer.tsx
@@ -35,94 +35,92 @@ export const TimerContainer: React.FC<TimerContainerProps> = ({
   };
 
   return (
-    <div className="App">
-      <div className="flex flex-col w-full h-[100vh] min-h-screen bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <div className="flex w-full h-[14%] " />
-        <Timer
-          setStartTime={setStartTime}
-          startCount={startCount}
-          resetCount={resetCount}
-          stopTimer={stopTimer}
-          setTimerDisable={setTimerDisable}
-          onEnd={(startTime: string) => {
-            setIsStop(true);
-            setIsRun(false);
+    <div className="flex flex-col w-full App h-screen min-h-screen bg-[#282c34] text-white text-[calc(10px+2vmin)] overflow-hidden	">
+      <div className="flex w-full h-[14%] " />
+      <Timer
+        setStartTime={setStartTime}
+        startCount={startCount}
+        resetCount={resetCount}
+        stopTimer={stopTimer}
+        setTimerDisable={setTimerDisable}
+        onEnd={(startTime: string) => {
+          setIsStop(true);
+          setIsRun(false);
 
-            addTimeList(startTime);
+          addTimeList(startTime);
+        }}
+      />
+      <div className="flex flex-row	w-full justify-center ">
+        <TimeButton
+          disable={timerDisable}
+          time={times[0]}
+          onClick={(num: number) => {
+            setStartTime.current(num);
+            setDefaultTime(num);
           }}
         />
-        <div className="flex flex-row	w-full justify-center ">
-          <TimeButton
-            disable={timerDisable}
-            time={times[0]}
-            onClick={(num: number) => {
-              setStartTime.current(num);
-              setDefaultTime(num);
-            }}
-          />
-          <TimeButton
-            disable={timerDisable}
-            time={times[1]}
-            onClick={(num: number) => {
-              setStartTime.current(num);
-              setDefaultTime(num);
-            }}
-          />
-          <TimeButton
-            disable={timerDisable}
-            time={times[2]}
-            onClick={(num: number) => {
-              setStartTime.current(num);
-              setDefaultTime(num);
-            }}
-          />
-        </div>
-        <div className="flex flex-row w-full justify-center">
+        <TimeButton
+          disable={timerDisable}
+          time={times[1]}
+          onClick={(num: number) => {
+            setStartTime.current(num);
+            setDefaultTime(num);
+          }}
+        />
+        <TimeButton
+          disable={timerDisable}
+          time={times[2]}
+          onClick={(num: number) => {
+            setStartTime.current(num);
+            setDefaultTime(num);
+          }}
+        />
+      </div>
+      <div className="flex flex-row w-full justify-center">
+        <Button
+          onClick={() => {
+            startCount.current();
+            setIsStop(false);
+            setIsRun(true);
+          }}
+          name="시작"
+          className={`w-28 h-28 m-5 rounded-full  ${buttonColor}`}
+          disable={timerDisable}
+        />
+        {!isStop ? (
           <Button
             onClick={() => {
-              startCount.current();
-              setIsStop(false);
-              setIsRun(true);
+              stopTimer.current();
+              setIsStop(true);
+              setIsRun(false);
             }}
-            name="시작"
+            name="정지"
             className={`w-28 h-28 m-5 rounded-full  ${buttonColor}`}
-            disable={timerDisable}
+            disable={!isRun}
           />
-          {!isStop ? (
-            <Button
-              onClick={() => {
-                stopTimer.current();
-                setIsStop(true);
-                setIsRun(false);
-              }}
-              name="정지"
-              className={`w-28 h-28 m-5 rounded-full  ${buttonColor}`}
-              disable={!isRun}
-            />
-          ) : (
-            <Button
-              onClick={() => {
-                resetCount.current();
-                setIsStop(false);
-                setStartTime.current(deafultTime);
-              }}
-              name="재설정"
-              className={`w-28 h-28 m-5 rounded-full bg-orange-500 ${buttonColor}`}
-            />
-          )}
-        </div>
-        <div className="flex-col w-full h-[300px] ">
-          <SetList times={timeList} />
-          <div className="w-full h-full">
-            <Button
-              onClick={() => {
-                resetTimeList();
-              }}
-              className={`w-[85%] h-1/5 mt-6 rounded-lg ${buttonColor}`}
-              name="초기화"
-              fontSize="text-[25px]"
-            />
-          </div>
+        ) : (
+          <Button
+            onClick={() => {
+              resetCount.current();
+              setIsStop(false);
+              setStartTime.current(deafultTime);
+            }}
+            name="재설정"
+            className={`w-28 h-28 m-5 rounded-full bg-orange-500 ${buttonColor}`}
+          />
+        )}
+      </div>
+      <div className="flex-col w-full h-[300px] ">
+        <SetList times={timeList} />
+        <div className="w-full h-full">
+          <Button
+            onClick={() => {
+              resetTimeList();
+            }}
+            className={`w-[85%] h-1/5 mt-6 rounded-lg ${buttonColor}`}
+            name="초기화"
+            fontSize="text-[25px]"
+          />
         </div>
       </div>
     </div>
