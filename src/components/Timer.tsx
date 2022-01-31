591eae811d65deb1eb621891e38b9df8d6a46a20
LimFull <full7002@naver.com>
2022-01-31 18:05:48 +0900

    useCallback

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, Dispatch, SetStateAction } from "react";
+import React, {
+  useEffect,
+  useRef,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+} from "react";
 import { getFormattedTime } from "../utils";
 import beepbeep from "../assets/sounds/beepbeep.mp3";
 
@@ -21,28 +27,7 @@ export const Timer: React.FC<TimerProps> = ({
   const interval = useRef(0);
   const audio = useRef(new Audio());
 
-  useEffect(() => {
-    startCount.current = startCountDown;
-    resetCount.current = resetCountDown;
-    stopTimer.current = stopCountDown;
-    setStartTime.current = _setStartTime;
-  }, [
-    resetCount,
-    resetCountDown,
-    setStartTime,
-    startCount,
-    startCountDown,
-    stopCountDown,
-    stopTimer,
-  ]);
-
-  function _setStartTime(time: number = 0): void {
-    timerStartTime.current = time;
-    const p = document.getElementById("time")! as HTMLParagraphElement;
-    p.innerText = String(getFormattedTime(time));
-  }
-
-  function startCountDown(): void {
+  const startCountDown = useCallback((): void => {
     audio.current = new Audio();
     var currentTime = timerStartTime.current;
     if (currentTime <= 0) {
@@ -64,18 +49,81 @@ export const Timer: React.FC<TimerProps> = ({
         p.innerText = String(getFormattedTime(currentTime));
       }
     }, intervalTime);
-  }
+  }, [setTimerDisable]);
 
-  function resetCountDown(): void {
+  const resetCountDown = useCallback((): void => {
     setTimerDisable(false);
 
     window.clearInterval(interval.current);
-  }
+  }, [setTimerDisable]);
 
-  function stopCountDown(): void {
+  const stopCountDown = useCallback((): void => {
     setTimerDisable(false);
     window.clearInterval(interval.current);
-  }
+  }, [setTimerDisable]);
+
+  const _setStartTime = useCallback((time: number = 0): void => {
+    timerStartTime.current = time;
+    const p = document.getElementById("time")! as HTMLParagraphElement;
+    p.innerText = String(getFormattedTime(time));
+  }, []);
+
+  useEffect(() => {
+    startCount.current = startCountDown;
+    resetCount.current = resetCountDown;
+    stopTimer.current = stopCountDown;
+    setStartTime.current = _setStartTime;
+  }, [
+    resetCount,
+    resetCountDown,
+    setStartTime,
+    startCount,
+    startCountDown,
+    stopCountDown,
+    stopTimer,
+    _setStartTime,
+  ]);
+
+  // function _setStartTime(time: number = 0): void {
+  //   timerStartTime.current = time;
+  //   const p = document.getElementById("time")! as HTMLParagraphElement;
+  //   p.innerText = String(getFormattedTime(time));
+  // }
+
+  // function startCountDown(): void {
+  //   audio.current = new Audio();
+  //   var currentTime = timerStartTime.current;
+  //   if (currentTime <= 0) {
+  //     return;
+  //   }
+  //   setTimerDisable(true);
+  //   const p = document.getElementById("time")! as HTMLParagraphElement;
+  //   const intervalTime = 10;
+  //   interval.current = window.setInterval(() => {
+  //     currentTime -= intervalTime;
+  //     if (currentTime <= 0) {
+  //       p.innerText = String(getFormattedTime(0));
+  //       window.clearInterval(interval.current);
+  //       setTimerDisable(false);
+
+  //       audio.current.src = beepbeep;
+  //       audio.current.play();
+  //     } else {
+  //       p.innerText = String(getFormattedTime(currentTime));
+  //     }
+  //   }, intervalTime);
+  // }
+
+  // function resetCountDown(): void {
+  //   setTimerDisable(false);
+
+  //   window.clearInterval(interval.current);
+  // }
+
+  // function stopCountDown(): void {
+  //   setTimerDisable(false);
+  //   window.clearInterval(interval.current);
+  // }
 
   return (
     <div>
