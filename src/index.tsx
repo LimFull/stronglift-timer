10e81623c38f024052d6076959f846db617c8a0f
amp-pool <pool@amplabs.co>
2022-08-04 10:12:29 +0900

    styled-component

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter as Router} from "react-router-dom";
-import BottomNav from "./components/BottomNav";
 
 ReactDOM.render(
     <React.StrictMode>
